7434cda24a696964751060181a046270e0433203
Boris Nimcevic <borisnimcevic@gmail.com>
2022-11-15 12:01:28 +0100

    references

---
+++ rust/references/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let mut my_number = 8;
+    let num_ref = &mut my_number;
+    *num_ref += 10; // Use * to change the i32 value.
+    println!("{}", my_number);
+
+    let second_number = 800;
+    let triple_reference = &&&second_number;
+    println!(
+        "Second_number = triple_reference? {}",
+        second_number == ***triple_reference
+    );
+
+    let mut num = 10;
+    let mut_ref_num = &mut num;
+    *mut_ref_num += 10;
+    let ref_num = &num;
+    println!("{}", ref_num);
+
+    let country = String::from("Austria"); // Now we have a String called country
+    let country_ref = &country; // country_ref is a reference to this data. It's not going to change
+    let country = 8; // Now we have a variable called country that is an i8. But it has no relation to the other one, or to country_ref
+    println!("{}, {}", country_ref, country); // country_ref still refers to the data of String::from("Austria") that we gave it.
+}
